44ffdac81339e8d7a1f35d049ed63dcd357ed9fd
Prakhar7755 <prakhar.5779.aps3@gmail.com>
2026-05-22 18:55:00 +0530

    feat: setup database connection

---
+++ src/models/user.model.js
@@ -0,0 +1,11 @@
+import { integer, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
+
+export const usersTable = pgTable('users', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  password: varchar({ length: 255 }).notNull().notNull(),
+  role: varchar({ length: 50 }).notNull().default('user'),
+  created_at: timestamp('created_at').notNull().defaultNow(),
+  updated_at: timestamp('updated_at').notNull().defaultNow(),
+});
